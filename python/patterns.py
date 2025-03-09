6adf3f5127f99243a379dbd16ec61a47ca22c757
shivamo7 <shivamvarshney@antiersolutions.com>
2025-03-09 20:49:35 +0530

    patterns

--- python/patterns.py
+++ python/patterns.py
@@ -17,11 +17,50 @@
 # * * *
 # * * * *
 
+# n = int(input("Enter the number of rows "))
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         if(j <= i) : 
+#             print("*",end = " ")
+#         else:
+#             print(" ",end = " ")
+#     print()
+
+
+
+# *   *
+#  * * 
+#   *  
+#  * * 
+# *   *
+
+
+
+# n = int(input("Enter the number of rows "))
+# for i in range(0,n):
+#     for j in range(0,n):
+#         if i == j or j == n-1-i:
+#             print("*",end="")
+#         else:
+#             print(" ",end="")
+#     print()
+
+
+
+#     *    
+#    ***   
+#   *****  
+#  ******* 
+# *********
+
+
 n = int(input("Enter the number of rows "))
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        if(j <= i) : 
-            print("*",end = " ")
+m = 2*n-1
+
+for i in range(0,n) :
+    for j in range(0,m):
+        if j >= n-1-i and j <= n-1+i : 
+            print("*",end="")
         else:
-            print(" ",end = " ")
+            print(" ",end="")
     print()
