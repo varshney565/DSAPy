b7c65819ffbdd1735d05b8f2611af7144d66c649
shivamo7 <shivamvarshney@antiersolutions.com>
2025-03-18 21:36:25 +0530

    bits

---
+++ python/numbersystem.py
@@ -0,0 +1,33 @@
+# n = int(input("Enter the number"))
+
+# temp = []
+
+# while n > 0:
+#     remainder = n%2
+#     temp.append(remainder)
+#     n = n//2
+
+# print(temp)
+
+
+# ans = ''
+# for i in range(len(temp)-1, -1,-1):
+#     ans = ans + str(temp[i])
+
+# print(ans)
+
+
+
+# n = 12
+# k = n >> 3
+# print(k)
+
+
+a = 11
+b = 2
+c = a&b
+print(c)
+
+
+a = 222222222222222222222222222222222222222222222222222222222222222222222222222222222222 * 222222222222222222222222222222222222222222222222222222222222222222222222222222222222
+print(a)
