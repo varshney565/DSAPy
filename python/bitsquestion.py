e724dbd78ff8a3b98ee0f978049cd51af23c4f93
varshney565 <uietcse1923shivamvarshney@kuk.ac.in>
2025-03-22 21:06:09 +0530

    bits questions

--- python/bitsquestion.py
+++ python/bitsquestion.py
@@ -13,11 +13,36 @@
 
 #check kth bit is set or not
 
-n = int(input("Enter the number"))
-k = int(input("Enter the bit position"))
+# n = int(input("Enter the number"))
+# k = int(input("Enter the bit position"))
 
-if n & (1 << k) == 0 :
-    print("Bit is not set")
-else:
-    print("Bit is set")
+# if n & (1 << k) == 0 :
+#     print("Bit is not set")
+# else:
+#     print("Bit is set")
 
+
+
+# count the number of set bits
+# n = int(input("Enter the number"))
+
+# ans = 0
+# while n > 0 : 
+#     if n&1 == 1:
+#         ans += 1
+#     n = n >> 1
+
+# print(ans)
+
+
+
+
+
+input = [2,2,5,5,1,1,9,9,9,8,8]
+
+res = 0
+
+for i in range(0,len(input)):
+    res = res^input[i]
+
+print(res)
